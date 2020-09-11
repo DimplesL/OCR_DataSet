8db55415713265f93f9174f29a9142c13cad9a83
qiuyurui <qiuyurui@guazi.com>
2020-09-11 17:52:57 +0800

    add new

--- convert/getMixData.py
+++ convert/getMixData.py
@@ -12,7 +12,7 @@ import pathlib
 import os
 import sys
 import random
-
+import json
 sys.path.append(os.getcwd())
 from tqdm import tqdm
 import cv2
@@ -144,6 +144,53 @@ def icdar2015cvt(gt_dict, data_root, gt_path, img_folder):
             cur_gt['annotations'].append(cur_line_gt)
         data_list.append(cur_gt)
     gt_dict['data_list'].extend(data_list)
+
+
+def LabelMe2cvt(gt_dict, gt_dict2, data_root, gt_folder):
+    """
+    将labelme格式的gt转换为json格式
+    :param gt_folder:
+    :param gt_dict:
+    :return:
+    """
+    json_files = get_file_list(os.path.join(data_root, gt_folder), p_postfix=['.json'])
+    random.shuffle(json_files)
+    all_cnt = len(json_files)
+    train_files = json_files[:int(all_cnt * 0.9)]
+    val_files = json_files[int(all_cnt * 0.9):]
+    data_list = []
+    for json_file in tqdm(train_files):
+        img_path = pathlib.Path(json_file)
+        cur_gt = {'img_name': gt_folder + '/' + img_path.name.replace('.json', '.jpg'), 'annotations': []}
+        content = json.load(open(json_file, 'r'))
+        for shape in content['shapes']:
+            cur_line_gt = {'polygon': [], 'text': '', 'illegibility': False, 'language': 'Latin'}
+            chars_gt = [{'polygon': [], 'char': '', 'illegibility': False, 'language': 'Latin'}]
+            cur_line_gt['chars'] = chars_gt
+            # 字符串级别的信息
+            cur_line_gt['polygon'] = shape['points']
+            cur_line_gt['text'] = "###"
+            cur_line_gt['illegibility'] = True if cur_line_gt['text'] == '*' or cur_line_gt['text'] == '###' else False
+            cur_gt['annotations'].append(cur_line_gt)
+        data_list.append(cur_gt)
+    gt_dict['data_list'].extend(data_list)
+
+    data_list = []
+    for json_file in tqdm(val_files):
+        img_path = pathlib.Path(json_file)
+        cur_gt = {'img_name': gt_folder + '/' + img_path.name.replace('.json', '.jpg'), 'annotations': []}
+        content = json.load(open(json_file, 'r'))
+        for shape in content['shapes']:
+            cur_line_gt = {'polygon': [], 'text': '', 'illegibility': False, 'language': 'Latin'}
+            chars_gt = [{'polygon': [], 'char': '', 'illegibility': False, 'language': 'Latin'}]
+            cur_line_gt['chars'] = chars_gt
+            # 字符串级别的信息
+            cur_line_gt['polygon'] = shape['points']
+            cur_line_gt['text'] = "###"
+            cur_line_gt['illegibility'] = True if cur_line_gt['text'] == '*' or cur_line_gt['text'] == '###' else False
+            cur_gt['annotations'].append(cur_line_gt)
+        data_list.append(cur_gt)
+    gt_dict2['data_list'].extend(data_list)
 
 
 if __name__ == '__main__':
